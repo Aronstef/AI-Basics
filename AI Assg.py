1ed85c7c7c90560c4be8111b46267df48fd29524
Aronstef <158732539+Aronstef@users.noreply.github.com>
2024-02-05 10:48:11 +0530

    Add files via upload
    AI Assignment on basics of AI.

---
+++ AI Assg.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+pip install tensorflow
+
+
+# In[3]:
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input,Dense
+
+
+# In[4]:
+
+
+# Model Initialisation
+model = Sequential()
+
+
+# In[5]:
+
+
+pip install Scipy
+
+
+# In[6]:
+
+
+pip install -U scikit-learn
+
+
+# In[7]:
+
+
+pip install TFlearn
+
+
+# In[ ]:
+
+
+
+
